1dea6ce9060b2a947680b329a90486ec0ce73a66
vojtech <vojtech.sedlecky@andritz.com>
2018-05-17 21:05:47 +0200

    add droidload.py

---
+++ src/python/droidload.py
@@ -0,0 +1,69 @@
+import os.path
+import sys
+import time
+
+path = 'a/btspp2file/'
+
+def btspp2file_write(data):
+    try:
+        timeout = 0
+        while os.path.exists(os.path.join(path, 'tx.txt')):
+            time.sleep(0.1)
+            timeout = timeout + 1
+            if timeout > 40:
+               raise Exception('btspp2file write timeout')
+
+        with open(os.path.join(path, 'tx.txt'), 'a') as outfile:
+             outfile.write(data + '\n')
+    except Exception as e: 
+        print(e)
+
+def btspp2file_read():
+    try:
+        timeout = 0
+        while not os.path.exists(os.path.join(path, 'rx.txt')):
+            time.sleep(0.1)
+            timeout = timeout + 1
+            if timeout > 40:
+               raise Exception('btspp2file read timeout')
+        with open(os.path.join(path, 'rx.txt'), 'r') as infile:
+            data = ''
+            for line in infile:
+                data =  data + line
+        return data        
+        
+    except Exception as e: 
+        print(e)
+
+    try:    
+        os.remove(os.path.join(path, 'rx.txt'))
+    except:
+        pass
+        
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        fileName = sys.argv[1]
+    else:
+        exit()
+    
+    print(fileName)
+    with open(fileName) as file:  
+        for data in file: 
+            
+            
+            #g.ser.write('?') 
+            btspp2file_write('?')
+            line = btspp2file_read()
+            print line
+            while line == None or line[0:1] != '0':
+                time.sleep(0.1)
+                btspp2file_write('?')
+                line = btspp2file_read()
+                print line
+            
+            print data
+            btspp2file_write(data)
+            line = btspp2file_read()
+            print line
+        
